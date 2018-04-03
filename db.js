1f17dbecd19697c5a3c687ff6d55ed6f56061bf1
chasbeen <charlesavt@yahoo.com>
2018-04-03 16:34:53 +0100

    Initial commit of Base Rest Api

---
+++ db.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+//mongoose.connect('mongodb://C:/AGIT/angular-database/mongodb/bin --port 27017')
+//mongoose.connect('mongodb://username:password@host:port/database?options...');
+mongoose.connect('mongodb://localhost/angular-database');
